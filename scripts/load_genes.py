84da283cc876082c61835c39564133aac66fb33a
Sanjay Boddu <sboddu@ebi.ac.uk>
2021-08-05 13:20:08 +0100

    Return an empty phased_exons for non-coding transcripts

--- scripts/load_genes.py
+++ scripts/load_genes.py
@@ -324,8 +324,7 @@ def format_transcript(
                 '3_prime_utr': None,
                 'cds': None,
                 'product_id': None,
-                # Phased exons may have to be disabled
-                'phased_exons': common.utils.phase_exons(ordered_formatted_exons, transcript['id'], phase_info),
+                'phased_exons': [],
                 # We'll know default later when it becomes relevant
                 'default': defaults.pop(),
                 'cdna': common.utils.format_cdna(transcript=transcript, refget=refget, non_coding = True)
