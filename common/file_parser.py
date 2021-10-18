8c770ba392cdaf3265b5e1629e4dffe6af9f3167
Mehrnaz Charkhchi <mcharkhchi@ebi.ac.uk>
2021-10-18 15:00:31 +0100

    Remove exceptions.

--- common/file_parser.py
+++ common/file_parser.py
@@ -3,13 +3,10 @@ class ChromosomeChecksum:
         self.genome_id = genome_id
         self.flat_file = "/nfs/services/enswbsites/newsite/data/chromosome_checksums/" + self.genome_id + "/chrom.hashes"
         self.results_dict = {}
-        try:
-            with open(self.flat_file) as chrom_hashes_file:
-                for line in chrom_hashes_file:
-                    chr_id, md5, *_ = line.split()
-                    self.results_dict[chr_id] = md5
-        except FileNotFoundError:
-            raise FileNotFoundError(self.flat_file + " Not Found")
+        with open(self.flat_file) as chrom_hashes_file:
+            for line in chrom_hashes_file:
+                chr_id, md5, *_ = line.split()
+                self.results_dict[chr_id] = md5
 
     def get_checksum(self, chromosome_id):
         return self.results_dict.get(chromosome_id, None)
