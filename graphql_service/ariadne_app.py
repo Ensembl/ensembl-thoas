babd1cac35e3c1236801c2d270f281cb4f8daf4f
Alex Cockburn <ahc@ebi.ac.uk>
2022-09-07 16:42:54 +0100

    Fix bug where different requests are affecting each others' state

--- graphql_service/ariadne_app.py
+++ graphql_service/ariadne_app.py
@@ -50,11 +50,17 @@ def prepare_executable_schema() -> GraphQLSchema:
 def prepare_context_provider(context: Dict) -> Callable[[Request], Dict]:
     """
     Returns function for injecting context to graphql executors.
-    The context will contain a pre-configured DB client and other goodies.
+
+    context: The context objects that we want to inject to the graphql
+    executors.  The `context_provider` method is a closure, so the
+    `context` variable will be the same Python object for every request.
+    This means that it should only contain objects that we want to share
+    between requests, for example Mongo client, XrefResolver
     """
 
     def context_provider(request: Request) -> Dict:
-        context["request"] = request
-        return context
+        """We must return a new object with every request,
+        otherwise the requests will pollute each other's state"""
+        return {"request": request, **context}
 
     return context_provider
