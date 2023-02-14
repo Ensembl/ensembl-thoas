0d9e338023eda32a0c9c015364acb7fe18976319
Bilal <bilal@ebi.ac.uk>
2023-02-14 14:01:09 +0000

    fix formatting (black is pain in the back)

--- graphql_service/tests/snapshots/snap_test_gene_name_retrieval.py
+++ graphql_service/tests/snapshots/snap_test_gene_name_retrieval.py
@@ -7,70 +7,70 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_no_externaldb_source_id 1'] = {
-    'gene': {
-        'metadata': {
-            'name': {
-                'accession_id': 'A0A1D5TR86',
-                'source': None,
-                'url': None,
-                'value': 'Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]'
+snapshots["test_no_externaldb_source_id 1"] = {
+    "gene": {
+        "metadata": {
+            "name": {
+                "accession_id": "A0A1D5TR86",
+                "source": None,
+                "url": None,
+                "value": "Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]",
             }
         },
-        'stable_id': 'TraesCS2A02G142502'
+        "stable_id": "TraesCS2A02G142502",
     }
 }
 
-snapshots['test_no_externaldb_source_name 1'] = {
-    'gene': {
-        'metadata': {
-            'name': {
-                'accession_id': 'A0A1D5TR86',
-                'source': None,
-                'url': 'https://purl.uniprot.org/uniprot/A0A1D5TR86',
-                'value': 'Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]'
+snapshots["test_no_externaldb_source_name 1"] = {
+    "gene": {
+        "metadata": {
+            "name": {
+                "accession_id": "A0A1D5TR86",
+                "source": None,
+                "url": "https://purl.uniprot.org/uniprot/A0A1D5TR86",
+                "value": "Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]",
             }
         },
-        'stable_id': 'TraesCS2A02G142503'
+        "stable_id": "TraesCS2A02G142503",
     }
 }
 
-snapshots['test_no_xref_acc_id 1'] = {
-    'gene': {
-        'metadata': {
-            'name': {
-                'accession_id': None,
-                'source': {
-                    'description': 'Universal Protein Resource using Persistent URL system',
-                    'id': 'UniProtKB/TrEMBL',
-                    'name': 'UniProtKB/TrEMBL',
-                    'release': None,
-                    'url': 'https://www.uniprot.org/'
+snapshots["test_no_xref_acc_id 1"] = {
+    "gene": {
+        "metadata": {
+            "name": {
+                "accession_id": None,
+                "source": {
+                    "description": "Universal Protein Resource using Persistent URL system",
+                    "id": "UniProtKB/TrEMBL",
+                    "name": "UniProtKB/TrEMBL",
+                    "release": None,
+                    "url": "https://www.uniprot.org/",
                 },
-                'url': None,
-                'value': 'Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]'
+                "url": None,
+                "value": "Sulfotransferase [Source:UniProtKB/TrEMBL;Acc:A0A1D5TR86]",
             }
         },
-        'stable_id': 'TraesCS2A02G142500'
+        "stable_id": "TraesCS2A02G142500",
     }
 }
 
-snapshots['test_no_xref_description 1'] = {
-    'gene': {
-        'metadata': {
-            'name': {
-                'accession_id': 'A0A1D5TR86',
-                'source': {
-                    'description': 'Universal Protein Resource using Persistent URL system',
-                    'id': 'UniProtKB/TrEMBL',
-                    'name': 'UniProtKB/TrEMBL',
-                    'release': None,
-                    'url': 'https://www.uniprot.org/'
+snapshots["test_no_xref_description 1"] = {
+    "gene": {
+        "metadata": {
+            "name": {
+                "accession_id": "A0A1D5TR86",
+                "source": {
+                    "description": "Universal Protein Resource using Persistent URL system",
+                    "id": "UniProtKB/TrEMBL",
+                    "name": "UniProtKB/TrEMBL",
+                    "release": None,
+                    "url": "https://www.uniprot.org/",
                 },
-                'url': 'https://purl.uniprot.org/uniprot/A0A1D5TR86',
-                'value': None
+                "url": "https://purl.uniprot.org/uniprot/A0A1D5TR86",
+                "value": None,
             }
         },
-        'stable_id': 'TraesCS2A02G142501'
+        "stable_id": "TraesCS2A02G142501",
     }
 }
